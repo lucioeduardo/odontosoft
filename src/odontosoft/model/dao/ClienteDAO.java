e798a9a0a686c07339fec52b82ec021709cfd701
eduardo <eduardo@eduardo-Lenovo-G400s>
2016-12-16 19:41:01 -0300

    Adicionando pacotes MVC e iniciando a implementação da tela principal.

--- src/odontosoft/model/dao/ClienteDAO.java
+++ src/odontosoft/model/dao/ClienteDAO.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Model;
+package odontosoft.model.dao;
 
 /**
  *
  * @author Aluno
  */
-public class FuncionarioDAO {
+public class ClienteDAO {
     
 }
