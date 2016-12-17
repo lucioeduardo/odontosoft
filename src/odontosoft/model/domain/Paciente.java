25b842d293469a47b171f7c5ca5dac0bdd630ed2
lucioeduardo <eduardo.lucio.correia@gmail.com>
2016-12-17 15:36:14 -0300

    Paciente

--- src/odontosoft/model/domain/Paciente.java
+++ src/odontosoft/model/domain/Paciente.java
@@ -1,27 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package odontosoft.model.domain;
 
-import java.util.*;
-
-
-public class Cliente {
+public class Paciente {
     private String nome,cpf,telefone;
     private int idade;
-    private Endereco end;
     
-    public Cliente () {
+    public Paciente () {
         
     }
     
-    public Cliente (String nome, int idade, String cpf, Endereco end, String telefone) {
+    public Paciente (String nome, int idade, String cpf, String telefone) {
         this.nome = nome;
         this.idade = idade;
         this.cpf = cpf;
-        this.end = end;
         this.telefone = telefone;
     }
     
@@ -47,15 +37,6 @@ public class Cliente {
     
     public void setCpf(String cpf) {
         this.cpf = cpf;
-    }
-
-    
-    public Endereco getEnd() {
-        return end;
-    }
-    
-    public void setEnd(Endereco end) {
-        this.end = end;
     }
     
     public String getTelefone() {
