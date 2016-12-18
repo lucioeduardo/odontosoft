463f806d9d1db2865e6335b5a43a7983e53129d6
Nicholas Lima de Souza Silva <nicholassowza@hotmail.com>
2016-12-18 16:41:13 -0300

    Implementação de ProcedimentoDAO e remoção de código não usado em ConsultaDAO.

--- src/odontosoft/model/dao/ProcedimentoDAO.java
+++ src/odontosoft/model/dao/ProcedimentoDAO.java
@@ -5,7 +5,13 @@
  */
 package odontosoft.model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import odontosoft.model.database.ConexaoBanco;
 import odontosoft.model.domain.Procedimento;
 
 /**
@@ -13,30 +19,105 @@ import odontosoft.model.domain.Procedimento;
  * @author mikolaja
  */
 public class ProcedimentoDAO implements InterfaceGenericDAO<Procedimento, Integer>{
+    ConexaoBanco conexao;
+    Connection connect = conexao.getConexao();
+    PreparedStatement stmt = null;
+    Procedimento procedimento;
 
+    public ProcedimentoDAO(ConexaoBanco conexao, Procedimento procedimento) {
+        this.conexao = conexao;
+        this.procedimento = procedimento;
+    }
+    
     @Override
     public void inserir(Procedimento var) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "INSERT INTO Procedimento(descricao, preco) VALUES (?,?);";
+        try{
+            stmt = connect.prepareStatement(sql);
+            stmt.setString(1, procedimento.getDescricao());
+            stmt.setDouble(2, procedimento.getPreco());
+            
+            stmt.execute();
+            stmt.close();
+            System.out.println("Dados inseridos no banco de dados!");
+        }catch(SQLException e){
+            System.out.println("Error: "+e);
+        }
     }
 
     @Override
     public List<Procedimento> listar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ResultSet query;
+        List<Procedimento> list = new ArrayList<>();
+        String sql = "SELECT * FROM Procedimento";
+        try{
+            stmt = connect.prepareStatement(sql);
+            query = stmt.executeQuery();
+            
+            while(query.next()){
+                list.add(new Procedimento(query.getInt("id"), query.getString("descricao"), query.getDouble("preco")));
+                
+            }
+            query.close();
+            stmt.close();
+        }catch(SQLException e){
+            System.out.println("Error: " +e);
+        }
+        return list;
     }
 
     @Override
     public void delete(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "DELETE FROM Procedimento WHERE id = ?";
+        try{
+            stmt = connect.prepareStatement(sql);
+            stmt.setInt(1, id);
+            
+            stmt.execute();
+            stmt.close();
+            System.out.println("Dados deletados com sucesso!");
+        }catch(SQLException e){
+            System.out.println("Error: " +e);
+        }
     }
 
     @Override
     public void update(Integer id, Procedimento newVar) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "UPDATE Procedimento SET descricao = ?, preco = ? WHERE id = ?;";
+        try{
+            stmt = connect.prepareStatement(sql);
+            stmt.setString(1, newVar.getDescricao());
+            stmt.setDouble(2, newVar.getPreco());
+            stmt.setInt(3, id);
+            
+            stmt.execute();
+            stmt.close();
+            System.out.println("Dados atualizados!");
+        }catch(SQLException e){
+            System.out.println("Error: " +e);
+        }
     }
 
     @Override
     public Procedimento buscaPorId(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "SELECT * FROM Procedimento WHERE id = ?";
+        Procedimento var = null;
+        
+        try{
+            stmt = connect.prepareStatement(sql);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                var = new Procedimento(rs.getInt("id"), rs.getString("descricao"), rs.getDouble("preco"));
+            }
+            rs.close();
+            stmt.close();
+        }catch(SQLException e){
+            System.out.println("Error: " +e);
+        }
+        
+        return var;
     }
 
    
