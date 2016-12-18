379d8f23b22cfbfe6470373c80290acb63bd7977
lucioeduardo <eduardo.lucio.correia@gmail.com>
2016-12-17 22:06:31 -0300

    Usuario

--- src/odontosoft/model/domain/Usuario.java
+++ src/odontosoft/model/domain/Usuario.java
@@ -2,12 +2,24 @@ package odontosoft.model.domain;
 
 public class Usuario {
     private String id,senha;
-
-    public Usuario(String id, String senha) {
+    private int idFuncionario;
+    
+    public Usuario(String id, String senha, int idFuncionario) {
         this.id = id;
         this.senha = senha;
+        this.idFuncionario = idFuncionario;
     }
 
+    public int getIdFuncionario() {
+        return idFuncionario;
+    }
+
+    public void setIdFuncionario(int idFuncionario) {
+        this.idFuncionario = idFuncionario;
+    }
+
+    
+    
     public String getId() {
         return id;
     }
