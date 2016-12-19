77dabb572ee5b7319d19319d742d44d8f2f22000
Aluno <Aluno@169.254.0.86>
2016-12-19 15:08:21 -0300

    PacientesController

--- src/odontosoft/controller/PacientesController.java
+++ src/odontosoft/controller/PacientesController.java
@@ -6,8 +6,18 @@
 package odontosoft.controller;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import odontosoft.model.dao.PacienteDAO;
+import odontosoft.model.database.ConexaoBanco;
+import odontosoft.model.domain.Paciente;
 
 /**
  * FXML Controller class
@@ -19,9 +29,28 @@ public class PacientesController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
+    @FXML
+    private TableColumn tableColumnPacienteNome,tableColumnPacienteTelefone;
+    @FXML
+    private TableView<Paciente> tableViewPacientes;
+    
+    private final ConexaoBanco conexao = new ConexaoBanco();
+    private PacienteDAO pacienteDAO = new PacienteDAO(conexao);
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        carregarTableViewPacientes();
     }    
     
+    public void carregarTableViewPacientes(){
+        
+        
+        tableColumnPacienteNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        tableColumnPacienteTelefone.setCellValueFactory(new PropertyValueFactory<>("telefone"));
+        
+        List<Paciente> listPacientes = pacienteDAO.listar();
+        ObservableList<Paciente> observableListPacientes = FXCollections.observableArrayList(listPacientes);
+        tableViewPacientes.setItems(observableListPacientes);
+    }
 }
