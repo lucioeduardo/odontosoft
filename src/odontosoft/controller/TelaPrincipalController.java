648c906e77b1297979a63c353448fe59e9a8a205
lucioeduardo <eduardo.lucio.correia@gmail.com>
2016-12-18 21:10:03 -0300

    TelaPrincipalController

--- src/odontosoft/controller/TelaPrincipalController.java
+++ src/odontosoft/controller/TelaPrincipalController.java
@@ -6,10 +6,17 @@
 package odontosoft.controller;
 
 import java.net.URL;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import odontosoft.model.dao.FuncionarioDAO;
 import odontosoft.model.domain.Funcionario;
 import odontosoft.model.domain.Usuario;
@@ -23,13 +30,31 @@ public class TelaPrincipalController implements Initializable {
     @FXML
     private Usuario user;
     @FXML
-    private Label lblNome;
+    private Label lblNome,lblHorario;
+    @FXML
+    private ImageView imgViewSair;
+    
     
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
+        
+        Timer timer = new Timer();
+        
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                
+                System.out.println("Executou!");
+                
+                Platform.runLater(() -> lblHorario.setText(getHora()));
+                
+            }
+        }, 0, 1000);
+        
         
     }   
 
@@ -39,9 +64,38 @@ public class TelaPrincipalController implements Initializable {
 
     public void setUser(Usuario user) {
         this.user = user;
+        
+        //Funcionario f = new FuncionarioDAO().buscaPorId(user.getIdFuncionario());
+        
         lblNome.setText(user.getId());
     }
     
+    @FXML
+    public void imgViewSairMouseEntered(){
+        Image image = new Image(getClass().getResourceAsStream("/odontosoft/view/img/iconSairHover.png"));
+        imgViewSair.setImage(image);
+    }
     
+    @FXML
+    public void imgViewSairMouseExited(){
+        Image image = new Image(getClass().getResourceAsStream("/odontosoft/view/img/iconSairPadrao.png"));
+        imgViewSair.setImage(image);
+    }
+    
+    @FXML
+    public void imgViewSairMouseClicked(){
+        System.exit(0);
+    }
+    
+    
+    public String getHora(){
+        Calendar cal = new GregorianCalendar();
+        
+        int hora = cal.get(Calendar.HOUR_OF_DAY);
+        int min = cal.get(Calendar.MINUTE);
+        int sec = cal.get(Calendar.SECOND);
+        
+        return hora + ":" + min + ":" + sec;
+    }
     
 }
