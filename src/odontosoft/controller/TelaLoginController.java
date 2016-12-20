0dde8186e4ce0e5bb68730ecc7fe74a0ac650ee3
Nicholas Lima de Souza Silva <nicholassowza@hotmail.com>
2016-12-20 19:26:21 -0300

    Correção de necessidades locais

--- src/odontosoft/controller/TelaLoginController.java
+++ src/odontosoft/controller/TelaLoginController.java
@@ -78,8 +78,8 @@ public class TelaLoginController implements Initializable {
                 
                 Scene scene = new Scene(root);
                 stage.setScene(scene);
-                stage.setFullScreen(false);
-                stage.setResizable(true);
+                //stage.setFullScreen(false);
+                //stage.setResizable(true);
                 scene.getStylesheets().add(getClass().getResource("/odontosoft/view/css/bootstrap3.css").toExternalForm());
                 scene.getStylesheets().add(getClass().getResource("/odontosoft/view/css/TelaPrincipal.css").toExternalForm());
                 
