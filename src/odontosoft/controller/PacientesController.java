1782d60a0edf53a9f5073ffe15a73b6f37ccda68
lucioeduardo <eduardo.lucio.correia@gmail.com>
2016-12-20 10:28:52 -0300

    PacientesController

--- src/odontosoft/controller/PacientesController.java
+++ src/odontosoft/controller/PacientesController.java
@@ -5,22 +5,32 @@
  */
 package odontosoft.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import odontosoft.model.dao.PacienteDAO;
 import odontosoft.model.database.ConexaoBanco;
 import odontosoft.model.domain.Paciente;
+import odontosoft.model.domain.Usuario;
 
 /**
  * FXML Controller class
@@ -41,9 +51,13 @@ public class PacientesController implements Initializable {
     private Label lblNomePaciente,lblTelefonePaciente,lblCpfPaciente,lblDataNascimentoPaciente;
     @FXML
     private ImageView imgViewfotoCliente;
+    @FXML
+    private Button btnAdicionarPaciente,btnAlterarPaciente,btnRemoverPaciente;
+    
     
     private final ConexaoBanco conexao = new ConexaoBanco();
     private PacienteDAO pacienteDAO = new PacienteDAO(conexao);
+    private Paciente pacienteSelecionado = null;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -63,10 +77,46 @@ public class PacientesController implements Initializable {
     }
     
     public void selecionarItemTableViewPacientes(Paciente paciente){
+        this.pacienteSelecionado=paciente;
+        
         lblNomePaciente.setText(paciente.getNome());
         lblDataNascimentoPaciente.setText(paciente.getData().toString());
         lblCpfPaciente.setText(paciente.getCpf());
         lblTelefonePaciente.setText(paciente.getTelefone());
         imgViewfotoCliente.setImage(new Image(getClass().getResourceAsStream(paciente.getFoto())));
     }
+    
+    @FXML
+    public void btnRemoverPacienteClicked(){
+        if(pacienteSelecionado!=null){
+            pacienteDAO.delete(pacienteSelecionado.getId());
+            carregarTableViewPacientes();
+        }
+    }
+    
+    @FXML
+    public void btnAdicionarPacienteClicked(){
+        Stage modal = new Stage();
+        
+        try {
+            System.out.println(getClass().getResource("/odontosoft/view/FXMLTelaAdicionarPaciente.fxml"));
+            Parent parent = FXMLLoader.load(getClass().getResource("/odontosoft/view/FXMLTelaAdicionarPaciente.fxml"));
+            Scene scene = new Scene(parent);
+            modal.setScene(scene);
+            scene.getStylesheets().add(getClass().getResource("/odontosoft/view/css/bootstrap3.css").toExternalForm());
+        } catch (IOException ex) {
+            Logger.getLogger(PacientesController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        modal.setTitle("Cadastrar Paciente");
+        modal.centerOnScreen();
+        modal.initOwner(lblNomePaciente.getScene().getWindow());
+        modal.initModality(Modality.APPLICATION_MODAL);
+        modal.showAndWait();
+        
+        carregarTableViewPacientes();
+    }
+    
+    
+    
 }
