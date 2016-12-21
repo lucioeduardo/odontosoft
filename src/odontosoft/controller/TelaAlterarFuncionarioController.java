76f29bd069c875af2274ba80f0d5eb4f42a99233
Nicholas Lima de Souza Silva <nicholassowza@hotmail.com>
2016-12-21 02:29:00 -0300

    Funções da Interface Funcionário.

--- src/odontosoft/controller/TelaAlterarFuncionarioController.java
+++ src/odontosoft/controller/TelaAlterarFuncionarioController.java
@@ -6,8 +6,17 @@
 package odontosoft.controller;
 
 import java.net.URL;
+import java.sql.Date;
 import java.util.ResourceBundle;
+import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.stage.Stage;
+import odontosoft.model.dao.FuncionarioDAO;
+import odontosoft.model.database.ConexaoBanco;
 import odontosoft.model.domain.Funcionario;
 
 /**
@@ -17,6 +26,17 @@ import odontosoft.model.domain.Funcionario;
 public class TelaAlterarFuncionarioController implements Initializable{
     private Funcionario funcionario;
     
+    @FXML
+    private Button btnSalvar,btnCancelar;
+    
+    @FXML
+    private TextField txtFieldNomeFuncionario, txtFieldCpfFuncionario, txtFieldRgFuncionario, txtFieldSalarioFuncionario,
+            txtFieldTelefoneFuncionario;
+   @FXML
+   private CheckBox checkBoxGerente, checkBoxDentista;
+   @FXML
+   private DatePicker datePickerDataNascFuncionario;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -24,6 +44,43 @@ public class TelaAlterarFuncionarioController implements Initializable{
     
     public void setFuncionario(Funcionario funcionario){
         this.funcionario = funcionario;
+        txtFieldNomeFuncionario.setText(funcionario.getNome());
+        txtFieldCpfFuncionario.setText(funcionario.getCpf());
+        txtFieldRgFuncionario.setText(funcionario.getRg());
+        txtFieldSalarioFuncionario.setText(String.valueOf(funcionario.getSalario()));
+        txtFieldTelefoneFuncionario.setText(funcionario.getTelefone());
+        
+        datePickerDataNascFuncionario.setValue(funcionario.getDataNascimento().toLocalDate());
+        checkBoxGerente.setSelected(funcionario.isGerente());
+        checkBoxDentista.setSelected(funcionario.isDentista());
+    }
+    
+    @FXML
+    public void btnSalvarClicked(){
+        String nome = txtFieldNomeFuncionario.getText();
+        String cpf = txtFieldCpfFuncionario.getText();
+        String rg = txtFieldRgFuncionario.getText();
+        double salario = Double.parseDouble(txtFieldSalarioFuncionario.getText());
+        String telefone = txtFieldTelefoneFuncionario.getText();
+        Date data = Date.valueOf(datePickerDataNascFuncionario.getValue());
+        
+        boolean gerente = checkBoxGerente.isSelected();
+        boolean dentista = checkBoxDentista.isSelected();
+        
+        Funcionario f = new Funcionario(0, nome, cpf, rg, telefone, salario, data, gerente, dentista);
+        ConexaoBanco conexao = new ConexaoBanco();
+        FuncionarioDAO funcionarioDao = new FuncionarioDAO(conexao);
+        
+        funcionarioDao.update(funcionario.getId(), f);
+        
+        Stage stage = (Stage)btnSalvar.getScene().getWindow();
+        stage.close();
+    }
+    
+    @FXML
+    public void btnCancelarClicked(){
+        Stage stage = (Stage)btnCancelar.getScene().getWindow();
+        stage.close();
     }
     
 }
