3df1c9dd7e80ef2aa4747c6d3486d781b2cede54
Aluno <Aluno@192.168.103.122>
2016-12-22 16:06:18 -0300

    ConsultaDAO

--- src/odontosoft/model/dao/ConsultaDAO.java
+++ src/odontosoft/model/dao/ConsultaDAO.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package odontosoft.model.dao;
 
-/**
- *
- * @author Aluno
- */
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
