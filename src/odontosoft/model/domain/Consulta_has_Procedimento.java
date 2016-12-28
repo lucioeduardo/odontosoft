449dd8354ecf5f3b711a6992937f704d16d8bd64
lucioeduardo <eduardo.lucio.correia@gmail.com>
2016-12-27 21:36:42 -0300

    Consulta_has_Procedimento

--- src/odontosoft/model/domain/Consulta_has_Procedimento.java
+++ src/odontosoft/model/domain/Consulta_has_Procedimento.java
@@ -10,11 +10,12 @@ package odontosoft.model.domain;
  * @author mikolaja
  */
 public class Consulta_has_Procedimento {
-    private int idConsulta, idProcedimento;
+    private int idConsulta, idProcedimento,quantidade;
 
-    public Consulta_has_Procedimento(int idConsulta, int idProcedimento) {
+    public Consulta_has_Procedimento(int idConsulta, int idProcedimento, int quantidade) {
         this.idConsulta = idConsulta;
         this.idProcedimento = idProcedimento;
+        this.quantidade = quantidade;
     }
 
     public int getIdConsulta() {
@@ -32,5 +33,15 @@ public class Consulta_has_Procedimento {
     public void setIdProcedimento(int idProcedimento) {
         this.idProcedimento = idProcedimento;
     }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    
     
 }
