254182f039e3fc0827c18034c4a505a2be821ed5
mrcrowley616 <vinicyus346@gmail.com>
2016-12-29 10:57:37 -0300

    TelaAdicionarFuncionarioController

--- src/odontosoft/controller/TelaAdicionarFuncionarioController.java
+++ src/odontosoft/controller/TelaAdicionarFuncionarioController.java
@@ -6,11 +6,14 @@ import java.sql.Date;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 import odontosoft.model.dao.FuncionarioDAO;
 import odontosoft.model.dao.UsuarioDAO;
 import odontosoft.model.database.ConexaoBanco;
@@ -41,24 +44,32 @@ public class TelaAdicionarFuncionarioController implements Initializable{
         Date dataNasc = Date.valueOf(datePickerDataNascFuncionario.getValue());
         String cpf = txtFieldCpfFuncionario.getText();
         String telefone = txtFieldTelefoneFuncionario.getText();
-        String rg = txtFieldRgFuncionario.getText();
-        double salario = Double.parseDouble(txtFieldSalarioFuncionario.getText());
+        String rg = txtFieldRgFuncionario.getText();        
+        try {
+            double salario = Double.parseDouble(txtFieldSalarioFuncionario.getText());
+                     
         boolean gerente = checkBoxGerente.isSelected();
         boolean dentista = checkBoxDentista.isSelected();
         
-        ConexaoBanco conexao = new ConexaoBanco();
-        
-        Funcionario f = new Funcionario(0, nome, cpf, rg, telefone, salario, dataNasc, gerente, dentista);
-        
-        FuncionarioDAO FuncionarioDao = new FuncionarioDAO(conexao);
+        ConexaoBanco conexao = new ConexaoBanco();       
+        Funcionario f  = new Funcionario(0, nome, cpf, rg, telefone, salario, dataNasc, gerente, dentista);                        
+        FuncionarioDAO FuncionarioDao = new FuncionarioDAO(conexao);        
         FuncionarioDao.inserir(f);
-        
-        Usuario u = new Usuario(txtFieldUsuarioFuncionario.getText(), txtFieldSenhaFuncionario.getText(), FuncionarioDao.getId());
-        UsuarioDAO usuarioDao = new UsuarioDAO(conexao.getConexao());
+                    
+        Usuario u = new Usuario(txtFieldUsuarioFuncionario.getText(), txtFieldSenhaFuncionario.getText(), FuncionarioDao.getId());        
+        UsuarioDAO usuarioDao = new UsuarioDAO(conexao.getConexao());        
         usuarioDao.inserir(u);
-        
-        Stage stage = (Stage)btnSalvar.getScene().getWindow();
-        stage.close();
+            
+            Stage stage = (Stage)btnSalvar.getScene().getWindow();
+            stage.close();
+        } catch (Exception excecao) {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Informação de erro: "+excecao);
+            alert.setHeaderText("Dados errados");
+            alert.setContentText("Não use '.' ou '-' ou ','");
+            
+            alert.showAndWait();
+        }
     }   
     
     public void btnCancelarClicked(){
