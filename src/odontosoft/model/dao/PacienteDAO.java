8c67c1f2f0219ade2ba0a12604ff1c02ddc10301
lucioeduardo <eduardo.lucio.correia@gmail.com>
2016-12-29 12:03:29 -0300

    PacienteDAO

--- src/odontosoft/model/dao/PacienteDAO.java
+++ src/odontosoft/model/dao/PacienteDAO.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import odontosoft.model.database.ConexaoBanco;
